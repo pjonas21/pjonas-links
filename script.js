9a4bec0ca2e9707c0b1ea2047ba4497789c523ce
Paulo Jonas <paulojonas@pop-os.localdomain>
2023-02-16 17:16:31 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+
+/*     if (html.classList.contains('light')){
+        html.classList.remove('light')
+    } else {
+        html.classList.add('light')
+    } */
+
+    html.classList.toggle('light')
+
+    //const seletor = html.classList
+
+    //{seletor.contains('light') ? seletor.remove('light') : seletor.add('light')}
+}
